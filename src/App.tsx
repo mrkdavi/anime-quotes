a9e20ef83898e707ec60fea9bffa3044d0b0a43e
Marcus Davi S. Queiros <marcusdavi.soar@gmail.com>
2022-02-21 00:02:16 -0300

    Update app.tsx and app.css

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,24 @@
-import './App.css'
+import "./app.css"
+import React from "react"
+import { Quote } from "./container/Quote"
+
+const styles: {[key: string]: React.CSSProperties} = {
+  appContainer: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    height: "100vh",
+  }
+}
 
 function App() {
 
   return (
     <>
-      
+      <div className="main-background"></div>
+      <div style={styles.appContainer}>
+        <Quote />
+      </div>
     </>
   )
 }
